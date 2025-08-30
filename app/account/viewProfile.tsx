3a1a1a96c88f117e861f867e7ce9e8f5158b5354
revieqt <opsima.josh@gmail.com>
2025-08-30 13:43:52 +0800

    Update: started work on profile

--- app/account/viewProfile.tsx
+++ app/account/viewProfile.tsx
@@ -1,23 +1,28 @@
+import BackButton from '@/components/custom/BackButton';
+import ProBadge from '@/components/custom/ProBadge';
+import ViewImageModal from '@/components/custom/ViewImage';
 import GradientHeader from '@/components/GradientHeader';
+import HorizontalSections from '@/components/HorizontalSections';
 import OptionsPopup from '@/components/OptionsPopup';
+import { ThemedIcons } from '@/components/ThemedIcons';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
-import BackButton from '@/components/custom/BackButton';
-import ViewImageModal from '@/components/custom/ViewImage';
 import { useSession } from '@/context/SessionContext';
 import useChangeProfileImage from '@/hooks/useChangeProfileImage';
 import { useLocalSearchParams } from 'expo-router';
 import React, { useState } from 'react';
-import { Image, StyleSheet, View } from 'react-native';
-import ProBadge from '@/components/custom/ProBadge';
+import { Image, ScrollView, StyleSheet, TouchableOpacity, View } from 'react-native';
 
 
 export default function ProfileScreen() {
   const { userId } = useLocalSearchParams();
   const { session } = useSession();
   const sessionUser = session?.user;
+  
   let user = sessionUser;
   let isCurrentUser = true;
+  const userType = (user?.type === "traveler") ? "Traveler" : ((user?.type === "tourGuide") ? "Tour Guide" : "Admin");
+  const userDescription = ((user?.isProUser) ? "Pro" : "Basic") + " " + userType;
 
   // If userId is provided and does not match session user, prepare to fetch from Firebase (not implemented yet)
   if (userId && userId !== sessionUser?.id) {
@@ -31,67 +36,109 @@ export default function ProfileScreen() {
 
   return (
     <ThemedView style={{flex:1}}>
-      <ThemedView color='primary'>
-        <BackButton style={styles.backButton}/>
-        <GradientHeader/>
-        {user && (
-          <OptionsPopup
-            style={styles.profileImage}
-          >
+      <ScrollView style={{flex:1}}>
+        <ThemedView color='primary'>
+          <View style={styles.header}>
+            <BackButton/>
+            { isCurrentUser && (
+              <OptionsPopup
+                key="options"
+                options={[
+                  
+                ]}
+              >
+                <ThemedIcons
+                  library="MaterialCommunityIcons"
+                  name="dots-vertical"
+                  size={24}
+                />
+              </OptionsPopup>
+            )}
+          </View>
+          <GradientHeader/>
+          
+          {user && (
+            <OptionsPopup
+              style={styles.profileImage}
+            >
               <Image
                 source={{ uri: session?.user?.profileImage || 'https://ui-avatars.com/api/?name=User' }}
                 style={{flex: 1}}
               />
-          </OptionsPopup>
-        )}
-        <ViewImageModal
-          visible={viewImageVisible}
-          imageUrl={user?.profileImage || ''}
-          onClose={() => setViewImageVisible(false)}
-        />
+            </OptionsPopup>
+          )}
+          <ViewImageModal
+            visible={viewImageVisible}
+            imageUrl={user?.profileImage || ''}
+            onClose={() => setViewImageVisible(false)}
+          />
 
-        <View style={{marginTop: 200, alignItems: 'center'}}>
-          {
-            user ? (
+          <View style={{marginTop: 120, alignItems: 'center'}}>
+            {user ? (
               <>
                 <View style={{ flexDirection: 'row', alignItems: 'center', gap: 10 }}>
                   <ThemedText type="subtitle">{user.fname} {user.mname ? user.mname : ''}{user.lname}</ThemedText>
                   <ProBadge/>
                 </View>
                 
-                <ThemedText type="defaultSemiBold">@{user.username}</ThemedText>
+                <ThemedText type="defaultSemiBold" style={{opacity: .5}}>@{user.username}</ThemedText>
+                <ThemedText style={{opacity: .5, marginBottom: 10}}>{userDescription}</ThemedText>
+              </>
+              ) : (
+                <ThemedText type="error">User not found.</ThemedText>
+              )
+            }
+          </View>
+
+          <View style={styles.bio}>
+            {(user?.bio?.trim() !== "") && (
+              <ThemedText>{user?.bio}</ThemedText>
+            )}
+            { isCurrentUser && (
+              <TouchableOpacity style={styles.editBio}>
+                <ThemedIcons library="MaterialIcons" name="edit" size={15}/>
+                <ThemedText>Edit Bio</ThemedText>
+              </TouchableOpacity>
+            )}
+          </View>
+        </ThemedView>
+        
+        <HorizontalSections
+          labels={['Travels', 'About']}
+          type="fullTab"
+          containerStyle={{ flex: 1}}
+          sections={[
+          <View key="travels" style={{ flex: 1, justifyContent: 'center', alignItems: 'center', height: 1000}}>
+            <View>
+            </View>
+          </View>,
+          <View key="about" style={{ flex: 1, padding: 20}}>
+            {user ? (
+              <>
+                <ThemedText>Email: {user.email}</ThemedText>
+                <ThemedText>Age: {user.age}</ThemedText>
+                <ThemedText>Gender: {user.gender}</ThemedText>
+                <ThemedText>Contact Number: {user.contactNumber}</ThemedText>
+                <ThemedText>Status: {user.status}</ThemedText>
+                <ThemedText>Type: {user.type}</ThemedText>
               </>
             ) : (
               <ThemedText type="error">User not found.</ThemedText>
-            )
-          }
-        </View>
-      </ThemedView>
-      
-      <View style={{paddingHorizontal: 20, paddingTop: 5}}>
-        {user ? (
-          <>
-            <ThemedText>Email: {user.email}</ThemedText>
-            <ThemedText>Age: {user.age}</ThemedText>
-            <ThemedText>Gender: {user.gender}</ThemedText>
-            <ThemedText>Contact Number: {user.contactNumber}</ThemedText>
-            <ThemedText>Status: {user.status}</ThemedText>
-            <ThemedText>Type: {user.type}</ThemedText>
-          </>
-        ) : (
-          <ThemedText type="error">User not found.</ThemedText>
-        )}
-      </View>
+            )}
+          </View>]}
+        />
+      </ScrollView>
     </ThemedView>
   );
 }
 
 const styles = StyleSheet.create({
-  backButton:{
-    position: 'absolute',
-    zIndex: 100,
-    top: 16,
-    left: 16,
+  header:{
+    justifyContent: 'space-between',
+    flexDirection: 'row',
+    alignItems: 'center',
+    padding: 20,
+    zIndex: 100
   },
   profileImage: {
     width: 120,
@@ -100,12 +147,24 @@ const styles = StyleSheet.create({
     borderWidth: 3,
     borderColor: 'white',
     alignSelf: 'center',
-    marginTop: 65,
     position: 'absolute',
+    marginTop: -20,
     zIndex: 100,
     overflow: 'hidden',
   },
-  content:{
-
+  bio:{
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '100%',
+  },
+  editBio: {
+    flexDirection: 'row',
+    gap: 5,
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingVertical:5,
+    paddingHorizontal: 10,
+    borderRadius: 30,
+    backgroundColor: '#ccc4 ',
   }
 });
